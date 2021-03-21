9a7cccc288bbc4cf818fea6ec08f2901ec04d25d
oussamaM1 <makhloukoussama1@gmail.com>
2021-03-21 18:31:19 +0100

    1480.RunningSumOf1dArray

---
+++ Java/src/1480. Running Sum of 1d Array/RunningSumOf1dArray.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int ans = 0 ; 
+        for(int i =0 ; i < nums.length ; i++){
+            ans += nums[i];
+            nums[i] = ans;
+        }
+        return nums;
+    }
+}
