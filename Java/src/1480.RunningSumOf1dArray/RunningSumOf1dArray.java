08149709fc215ce85350e6e720daf420bb6fa593
oussamaM1 <makhloukoussama1@gmail.com>
2021-03-21 18:49:37 +0100

    1480.RunningSumOf1dArray

--- Java/src/1480.RunningSumOf1dArray/RunningSumOf1dArray.java
+++ Java/src/1480.RunningSumOf1dArray/RunningSumOf1dArray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int ans = 0 ; 
-        for(int i =0 ; i < nums.length ; i++){
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
             ans += nums[i];
             nums[i] = ans;
         }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # leetCode
+
+| #   | Title | Solution |
+| --- | ----- | -------- |
+|1480|[Running Sum Of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array/) | [Java](./Java/src/1480.RunningSumOf1dArray/RunningSumOf1dArray.java)
